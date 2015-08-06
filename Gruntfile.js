9f3040e73403ba3dc4534cf22e4f14b09bbf359f
breeze4 <ambiturner0@gmail.com>
2015-08-05 21:57:21 -0700

    added gruntfile and configured build

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function (grunt) {
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-react');
+
+    grunt.initConfig({
+        react: {
+            jsx: {
+                files: {
+                    'resources/build/js/app.js': ['resources/js/**/*.jsx']
+                }
+            }
+        },
+
+        copy: {
+            lib: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'node_modules/react/dist/',
+                        src: ['react.js'],
+                        dest: 'resources/lib/'
+                    }
+                ]
+            },
+            public: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'resources/',
+                        src: ['index.html'],
+                        dest: 'public/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'resources/build/',
+                        src: ['js/**/*.js', '**/*.html'],
+                        dest: 'public/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'resources/lib/',
+                        src: ['**/*.js'],
+                        dest: 'public/js'
+                    }
+                ]
+            }
+        },
+
+        watch: {
+            react: {
+                files: ['resources/js/**.jsx'],
+                tasks: ['react']
+            }
+        }
+
+    });
+
+    // Default task(s).
+    grunt.registerTask('default', ['build', 'watch']);
+    grunt.registerTask('build', ['react', 'copy']);
+
+};
--- src/collabify/core.clj
+++ src/collabify/core.clj
@@ -2,8 +2,23 @@
   (:require
     [compojure.core :refer :all]
     [compojure.route :as route]
+    [ring.middleware.file :refer [wrap-file]]
+    [ring.middleware.resource :refer [wrap-resource]]
+    [ring.middleware.content-type :refer [wrap-content-type]]
+    [ring.middleware.not-modified :refer [wrap-not-modified]]
+    [ring.util.response :refer [file-response resource-response redirect]]
     ))
 
-(defroutes app
-           (GET "/" [] "<h1>Hello World</h1>")
+(defroutes app-routes
+           (GET "/" [] (file-response "index.html" {:root "resources/public"}))
+           ;(GET "/js/react.js" [] (file-response "react.js" {:root "resources/public/js"}))
+           ;(GET "/js" [] (resource-response "/*" {:root "resources/public/js"}))
+           ;(GET "/js/*" [] (resource-response "app.js" {:root "resources/public/js"}))
+           ;(route/resources "/" {:root "resources/public"})
            (route/not-found "<h1>Page not found</h1>"))
+
+(def app
+  (-> app-routes
+      (wrap-file "public")
+      ;(wrap-content-type)
+      (wrap-not-modified)))
